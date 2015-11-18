afe464b09929b67ed8ef759fc8e2238fcd44a54d
Toby Dylan Hocking <tdhock5@gmail.com>
2015-11-17 19:20:36 -0500

    first leaflet

--- map.R
+++ map.R
@@ -1,7 +1,5 @@
 works_with_R("3.2.2",
              data.table="1.9.6",
-             "tdhock/ggplot2@a8b06ddb680acdcdbd927773b1011c562134e4d2",
-             ggmap="2.5.2",
              leaflet="1.0.0")
 
 load("places.RData")
@@ -11,17 +9,44 @@ prices <- fread("products/hemp_seeds.csv")
 ## https://rstudio.github.io/leaflet/shapes.html
 
 store.manu <- unique(prices[!grepl("web", store),.(store, manufacturer)])
-places.dt[, place.type := NA_character_]
 setkey(places.dt, place.name)
 
 price.place.vec <- unique(store.manu[, c(store, manufacturer)])
 price.place.dt <- places.dt[price.place.vec]
 stopifnot(is.finite(price.place.dt$lon))
 
-for(ptype in c("store", "manufacturer")){
-  place.vec <- store.manu[[ptype]]
-  places.dt[place.vec, place.type := ptype]
+m <- leaflet() %>%
+  addTiles() %>%
+  addLayersControl(
+    overlayGroups=unique(places.dt$place.type),
+    options = layersControlOptions(collapsed = FALSE)
+    )
+setkey(places.dt, place.type)
+color.code <- c(
+  manufacture="#1B9E77",
+  consumer="#D95F02",
+  store="#7570B3")
+places.dt[, popup := sprintf("%s (%s)", place.name, place.type)]
+for(place.type in unique(places.dt$place.type)){
+  one.place <- places.dt[place.type]
+  m <- addCircleMarkers(
+    m, data=one.place,
+    color=color.code[[place.type]],
+    group=place.type,
+    popup=one.place$popup)
 }
-  
 
-addPolylines
+setkey(places.dt, place.name)
+for(path.i in 1:nrow(store.manu)){
+  one.path.row <- store.manu[path.i,]
+  one.path <- unique(c("Chez nous", unlist(one.path.row)))
+  path.dt <- places.dt[one.path]
+  m <-
+    path.dt[, {
+    addPolylines(m, lon, lat)
+    }]
+}
+
+## TODO:
+## https://rstudio.github.io/leaflet/markers.html#customizing-marker-icons
+## for question marks where we don't know.
