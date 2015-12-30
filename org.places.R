df5cf82f03585bd59ccd7184f0155245c995471b
Toby Dylan Hocking <tdhock5@gmail.com>
2015-12-30 18:23:34 -0500

    combine price tables

--- org.places.R
+++ org.places.R
@@ -19,12 +19,31 @@ for(org.file in to.process$org.file){
   system(cmd)
 }
 
+price.fields <- c(
+  "product.file",
+  "date",
+  "organic.file",
+  "quantity",
+  "unit",
+  "CAD")
+
 org.places <- list()
+prices.list <- list()
 for(file.i in 1:nrow(mtime.dt)){
   mtime.row <- mtime.dt[file.i, ]
   objs <- load(mtime.row$RData.file)
   if(length(data.by.section)){
     org.file <- paste(mtime.row$org.file)
+    if(is.data.table(data.by.section$prices$table)){
+      has.field <- price.fields %in% names(data.by.section$prices$table)
+      field.vec <- price.fields[!has.field]
+      for(field in field.vec){
+        data.by.section$prices$table[[field]] <- NA
+      }
+      price.dt <- data.by.section$prices$table[, price.fields, with=FALSE]
+      prices.list[[org.file]] <-
+        data.table(place=org.file, price.dt)
+    }
     org.places[[org.file]] <- data.by.section
   }
 }
@@ -33,4 +52,6 @@ table(unlist(lapply(org.places, function(L)names(L$prices$table))))
 table(unlist(lapply(org.places, function(L)L$prices$table$product)))
 table(unlist(lapply(org.places, function(L)L$prices$table$product.file)))
 
-save(org.places, file="org.places.RData")
+org.prices <- do.call(rbind, prices.list)
+
+save(org.places, org.prices, file="org.places.RData")
